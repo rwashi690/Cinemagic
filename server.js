470218d5e5f7bb3471fde051300ba7b7f314297f
Rachel Elizabeth Washington <rwashington325@gmail.com>
2022-02-04 20:35:03 -0700

    Fixed an error in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
+//const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 //const helpers = require('./utils/helpers');
 
@@ -14,18 +14,18 @@ const PORT = process.env.PORT || 3001;
 //const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+  secret: 'password',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-  app.use(session(sess));
+app.use(session(sess));
 
-  //app.engine('handlebars', hbs.engine);
+//app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
