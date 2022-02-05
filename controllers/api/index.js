d7ad45997ef7d92ca0cef74bd33c46907123bc13
Rachel Elizabeth Washington <rwashington325@gmail.com>
2022-02-04 20:03:30 -0700

    Add the index.js files for api routes

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router =require('express').Router();
+const userRoutes = require('./user-routes');
+const searchRoutes = require('./search-routes');
+
+router.use('/users', userRoutes);
+router.use('/searches', searchRoutes);
+
+module.exports = router;
