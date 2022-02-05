2b624da2c2889e956dbbb74d73b2fc042005ff7a
Rachel Elizabeth Washington <rwashington325@gmail.com>
2022-02-04 19:37:11 -0700

    reverted master branch

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,1 @@
-const loginHandler = async (event) => {
-  event.preventDefault();
 
-  //Get values of name and email from HTML by targetting 0
-  const username = document.querySelector('#username_login').value.trim();
-  const password = document.querySelector('#password_login').value.trim();
-
-  if (username && password) {
-    const response = await fetch('/api/users/login', {});
-  }
-  if (response.ok) {
-    document.location.replace('/profile');
-  } else {
-    alert(response.statusText);
-  }
-};
-
-document.querySelector('.loginForm').addEventListener('submit', loginHandler);
