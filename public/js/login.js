eeba719acb6af974663b625651ee8c70c1835c67
Rachel Elizabeth Washington <rwashington325@gmail.com>
2022-02-05 11:35:14 -0700

    Adds functionality for login.js to complete logging in

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,21 @@
 const loginHandler = async (event) => {
   event.preventDefault();
 
-  //Get values of name and email from HTML by targetting 0
+  //Get values of name and email from HTML by targetting the ids
   const username = document.querySelector('#username_login').value.trim();
   const password = document.querySelector('#password_login').value.trim();
 
   if (username && password) {
-    const response = await fetch('/api/users/login', {});
-  }
-  if (response.ok) {
-    document.location.replace('/profile');
-  } else {
-    alert(response.statusText);
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body:JSON.stringify({username, password}),
+      headers: {'Content-Type':'application/json'},
+    });
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
   }
 };
 
