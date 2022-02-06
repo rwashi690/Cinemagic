15819ee96210f62ab019b8a23a73712e63ef008a
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2022-02-05 23:08:39 -0500

    trying to figure out this problem of sorting through the return data

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -2,6 +2,65 @@ const router = require('express').Router();
 const { UserTag, Movie } = require('../models');
 
 // get all Movie belong to User logged in
+// router.get('/:id', withAuth, async (req, res) => {
+//   try {
+//     const userTags = await UserTag.findAll({
+//       where: {
+//         user_id: req.params.id,
+//       }
+//     });
+
+//     if(!userTags) {
+//       res.status(200).json({ message: 'You have not added any movies to your collection!'});
+//       return;
+//     }
+
+//     const userMovies = await Movie.findAll({
+//       where: {
+//         id: userTags,
+//       }
+//     });
+
+//     res.status(200).json(userMovies);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get('/:id', withAuth, async (req, res) => {
+//   try {
+//     const userTags = await UserTag.findAll({
+//       where: {
+//         user_id: req.params.id,
+//       }
+//     });
+
+//     const userMovies = await UserTag.findAll({
+//       include: [
+//         {
+//           model: Movie,
+//           required: true,
+//         },
+//         {
+//           model: UserTag,
+//           required: true,
+//           include: {
+//             model: User,
+//             required: true,
+//             where: {
+//               id: userTags.map(movie => movie.user_id),
+//             },
+//           },
+//         },
+//       ],
+//     });
+
+//     res.status(200).json(userMovies);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 router.get('/:id', withAuth, async (req, res) => {
   try {
     const userTags = await UserTag.findAll({
@@ -15,11 +74,7 @@ router.get('/:id', withAuth, async (req, res) => {
       return;
     }
 
-    const userMovies = await Movie.findAll({
-      where: {
-        movie_id: userTags,
-      }
-    });
+    const userMovies = userTags.map(userTags => userTags.movie_id);
 
     res.status(200).json(userMovies);
   } catch (err) {
