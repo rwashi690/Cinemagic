71af15643116f796b336c5a36eb1f288786357cc
Rachel Elizabeth Washington <rwashington325@gmail.com>
2022-02-06 12:59:29 -0700

    Added code for signing up

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,10 @@
+//
 const loginHandler = async (event) => {
   event.preventDefault();
-
   //Get values of name and email from HTML by targetting the ids
   const username = document.querySelector('#username_login').value.trim();
   const password = document.querySelector('#password_login').value.trim();
-
+  //Check to see if a username and password were entered
   if (username && password) {
     const response = await fetch('/api/users/login', {
       method: 'POST',
@@ -18,9 +18,27 @@ const loginHandler = async (event) => {
     }
   }
 };
-
 document.querySelector('.loginForm').addEventListener('submit', loginHandler);
 
-//const signUpHandler =async (event)=>{
-//  event.preventDefault();
-//}
+const signupHandler =async (event)=>{
+  event.preventDefault();
+  const username = document.querySelector('#username_signup').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password_signup').value.trim();
+
+  if (username && email && password){
+    const response = await fetch ('/api/users/signup', {
+      method: 'POST',
+      body: JSON. stringify({username, email, password}),
+      headers: {'Content-Type':'application/json'},
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert((response.statusText));
+    }
+  }
+};
+
+document.querySelector('.signupForm').addEventListener('submit', signupHandler);
