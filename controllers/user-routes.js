4a1e3181f527118ffd9107661ecf51d8fb2b0473
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2022-02-05 21:33:29 -0500

    user routes is tentatively functional

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -1,3 +1,30 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { UserTag, Movie } = require('../models');
 
+// get all Movie belong to User logged in
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const userTags = await UserTag.findAll({
+      where: {
+        user_id: req.params.id,
+      }
+    });
+
+    if(!userTags) {
+      res.status(200).json({ message: 'You have not added any movies to your collection!'});
+      return;
+    }
+
+    const userMovies = await Movie.findAll({
+      where: {
+        movie_id: userTags,
+      }
+    });
+
+    res.status(200).json(userMovies);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
