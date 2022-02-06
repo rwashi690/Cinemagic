8cfb0fcdfb32566bc8e1a52e68c2f79f70d3e635
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2022-02-06 04:45:58 -0500

    tentative code added

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -5,8 +5,18 @@ const router = require('express').Router();
 const { User, UserTag, Movie } = require('../models');
 
 // import login authentication
-const withAuth = require('../utils/authenication');
+const withAuth = require('../utils/authentication');
 
+// make new UserTag (aka designate new Movie to existing User)
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newMovie = await Movie.create(req.body);
+    const newUserTag = await UserTag.create({ movie_id: newMovie.id, user_id: req.user.id });
 
+    res.status(200).json(newUserTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
