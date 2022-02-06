71ea7ed5025b2035c71f9979a33de844e731275b
Rachel Elizabeth Washington <rwashington325@gmail.com>
2022-02-06 16:27:50 -0700

    Further testing with Heroku

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${ PORT }`);
+});
 
 const hbs = exphbs.create({ helpers });
 
