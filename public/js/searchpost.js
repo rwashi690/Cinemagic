cafce291089e6cbe579f329986ae642195ca9a95
Nick Lyle <nicklyle2009@gmail.com>
2022-02-07 00:34:48 -0500

    updated searchpost.js and also added image posting to search and user handlebars

--- public/js/searchpost.js
+++ public/js/searchpost.js
@@ -1,6 +1,15 @@
+const searchRoutes = require('../../controllers/search-routes');
 
-// fetch(url,(huge)).then(function (response) )
-// fetch huge database, seed through API
-// pagination.  takes giant fetches, google it
-// add to user database
-// images? get from seeded database, 
+const newMovieHandler = async (event) => {
+ event.preventDefault();
+
+const movieSearch = document.querySelector('#movie-search').value.trim();
+
+if (movie-search) {
+const response=  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&&query=${movieSearch}&page=1`).then()
+
+}
+}
+document
+  .querySelector('.new-movie-form')
+  .addEventListener('click', newMovieHandler);
