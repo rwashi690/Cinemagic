66bcb3591d75da16b79ed2a54d0799c5beba78e1
Nick Lyle <nicklyle2009@gmail.com>
2022-02-06 22:39:20 -0500

    first commit

--- public/js/login.js
+++ public/js/login.js
@@ -1,50 +1,44 @@
-const loginForm = async (event) => {
+//Create javascript
+const loginHandler = async (event) => {
   event.preventDefault();
-
-  const username = document.querySelector("username-login").value;
-  const password = document.querySelector("#password-login").value;
-
+  //Get values of name and email from HTML by targetting the ids
+  const username = document.querySelector('#username_login').value.trim();
+  const password = document.querySelector('#password_login').value.trim();
+  //Check to see if a username and password were entered
   if (username && password) {
-    const response = await fetch("/api/users/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
+    const response = await fetch('/login', {
+      method: 'POST',
+      body:JSON.stringify({username, password}),
+      headers: {'Content-Type':'application/json'},
     });
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace("/profile");
+      document.location.replace('/user');
     } else {
       alert(response.statusText);
     }
   }
 };
 
-const signupForm = async (event) => {
+const signupHandler =async (event)=>{
   event.preventDefault();
+  const username = document.querySelector('#username_signup').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password_signup').value.trim();
 
-  const name = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-
-  if (name && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ name, email, password }),
-      headers: { "Content-Type": "application/json" },
+  if (username && email && password){
+    const response = await fetch ('/', {
+      method: 'POST',
+      body: JSON. stringify({username, email, password}),
+      headers: {'Content-Type':'application/json'},
     });
 
     if (response.ok) {
-      document.location.replace("/profile");
+      document.location.replace('/user');
     } else {
-      alert(response.statusText);
+      alert((response.statusText));
     }
   }
 };
 
-document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginForm);
-
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupForm);
+document.querySelector('.loginForm').addEventListener('submit', loginHandler);
+document.querySelector('.signupForm').addEventListener('submit', signupHandler);
