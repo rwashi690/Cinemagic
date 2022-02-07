ac7822d83d09e5b62c3845b93bd11bea7b248629
Rachel Elizabeth Washington <rwashington325@gmail.com>
2022-02-07 01:27:52 -0700

    Adds a working API request

--- public/js/searchpost.js
+++ public/js/searchpost.js
@@ -1,15 +1,36 @@
 const searchRoutes = require('../../controllers/search-routes');
-
-const newMovieHandler = async (event) => {
- event.preventDefault();
-
+const apikey = config.API_KEY;
 const movieSearch = document.querySelector('#movie-search').value.trim();
 
-if (movie-search) {
-const response=  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&&query=${movieSearch}&page=1`).then()
+const newMovieHandler = async (event) => {
+  event.preventDefault();
+  if(movieSearch){
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apikey}&language=en-US&query=${movieSearched}&page=1`, {
+      "method": "GET"
+    }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        //const {page, results, total_pages, total_results}= data;
+        //console.log(results)
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+};
 
-}
-}
-document
-  .querySelector('.new-movie-form')
-  .addEventListener('click', newMovieHandler);
+document.querySelector('.new-movie-form').addEventListener('click', newMovieHandler);
+
+
+
+// const newMovieHandler = async (event) => {
+//     event.preventDefault();
+//    const movieSearch = document.querySelector('#movie-search').value.trim();
+//    if (movie-search) {
+//    const response=  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&&query=${movieSearch}&page=1`).then()
+//    }
+//    }
