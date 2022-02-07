d096cd10a549b53475208256d3f16b11f88a7c30
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2022-02-06 22:46:29 -0500

    committing for committing sake

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,13 +1,41 @@
+// // remove movie from collection
+// const removeMovieHandler = async (event, movieID, userID) => {
+//   event.preventDefault();
+//   // make variable that holds the movie id of the movie that is being deleted by the click of a button
+//   const doomedId = movieID;
+//   //send that id through a fetch delete request
+//   const response = await fetch('/user/' + userID, {
+//     method: 'DELETE',
+//     body: doomedId,
+//   });
+
+//   if (response.ok) {
+//     document.location.replace('/user/' + userID);
+//   } else {
+//     alert('Failed to remove Movie');
+//   }
+// };
+
+// // add event listener to activate
+// document
+//   .querySelector('.removeMovie-button')
+//   .addEventListener('click', removeMovieHandler(movieID, userID));
+
 // remove movie from collection
 const removeMovieHandler = async (event) => {
   event.preventDefault();
-
-  const response = await fetch('/user/:id' , {
+  // make variable that holds the movie id of the movie that is being deleted by the click of a button
+  const doomedId = document.;
+  // make variable that holds the user id of the user who wants to delete the movie
+  const userID = req.sessions.userId;
+  //send that id through a fetch delete request
+  const response = await fetch('/user/' + userID, {
     method: 'DELETE',
+    body: doomedId,
   });
 
   if (response.ok) {
-    document.location.replace('/user/:id');
+    document.location.replace('/user/' + userID);
   } else {
     alert('Failed to remove Movie');
   }
@@ -16,4 +44,4 @@ const removeMovieHandler = async (event) => {
 // add event listener to activate
 document
   .querySelector('.removeMovie-button')
-  .addEventListener('click', removeMovieHandler);
+  .addEventListener('submit', removeMovieHandler);
