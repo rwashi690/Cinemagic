ea13f59be0cf7c1919f45c3b0bcf82adeb6a339e
Rachel Elizabeth Washington <rwashington325@gmail.com>
2022-02-07 18:55:55 -0700

    Sucessfully displays movie title, movie description, movied poster and an image of the streaming service

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,45 +4,45 @@ const loadAllMoviesHandler = async (event) => {
 
   //const userID = req.sessions.userId;
 
-  if (userID) {
-    const response = await fetch("/user/profile", {
-      method: 'GET',
-      body: userID,
-    });
+  // if (userID) {
+  //   const response = await fetch("/user/profile", {
+  //     method: 'GET',
+  //     body: userID,
+  //   });
 
-    if (response.ok) {
-      document.location.replace("/user/profile");
-    } else {
-      res.status(500).json({ message: 'Oops, something went wrong.' });
-    }
+  if (response.ok) {
+    document.location.replace("/user/profile");
+  } else {
+    res.status(500).json({ message: 'Oops, something went wrong.' });
   }
+  // }
 };
 
 // remove movie from collection
-const removeMovieHandler = async (event) => {
-  event.preventDefault();
-  // make variable that holds the movie id of the movie that is being deleted by the click of a button
-  const doomedId = document.querySelector('#movie-id').value.trim();
-  // make variable that holds the user id of the user who wants to delete the movie
-  const userID = req.sessions.userId;
-  //send that id through a fetch delete request
-  if (doomedId && userID) {
-    const response = await fetch("/user/profile", {
-      method: 'DELETE',
-      body: doomedId,
-    });
+// const removeMovieHandler = async (event) => {
+//   event.preventDefault();
+// make variable that holds the movie id of the movie that is being deleted by the click of a button
+//const doomedId = document.querySelector('#movie-id').value.trim();
+// make variable that holds the user id of the user who wants to delete the movie
+// const userID = req.sessions.userId;
+// //send that id through a fetch delete request
+// if (doomedId && userID) {
+//   const response = await fetch("/user/profile", {
+//     method: 'DELETE',
+//     body: doomedId,
+//   });
 
-    if (response.ok) {
-      document.location.replace("/user/profile");
-    } else {
-      alert('Failed to remove Movie');
-    }
-  }
-};
+// if (response.ok) {
+//   document.location.replace("/user/profile");
+// } else {
+//   alert('Failed to remove Movie');
+// }
+//}
+// };
 
 // add event listeners to activate
-document
-  .querySelector('.removeMovie-button')
-  .addEventListener('submit', removeMovieHandler);
+//document
+//  .querySelector('.removeMovie-button')
+//  .addEventListener('submit', removeMovieHandler);
 
 document.addEventListener('load', loadAllMoviesHandler);
--- public/js/searchpost.js
+++ public/js/searchpost.js
@@ -1,6 +1,5 @@
-
 // fetch(url,(huge)).then(function (response) )
 // fetch huge database, seed through API
 // pagination.  takes giant fetches, google it
 // add to user database
-// images? get from seeded database, 
+// images? get from seeded database,
